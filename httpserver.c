4ed713bb4a37451f81df22d704ffc52d0511e3de
samtru99 <samtrujillo1999@gmail.com>
2024-01-25 18:09:59 -0800

    initial working signal handling - still need to figure out to let threads finish when SIGTERM is called

--- httpserver.c
+++ httpserver.c
@@ -29,6 +29,10 @@
 #include <signal.h>
 #include <pthread.h>
 #include <semaphore.h>
+
+volatile sig_atomic_t exit_loop = 0;
+//pthread_t *th;
+int SOCKETFD;
 //Function to convert user input to a port socket
 uint16_t convert(char *value, int length) {
     char str_num[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
@@ -48,6 +52,14 @@ uint16_t convert(char *value, int length) {
         new_val += (base_num * int_nums[find_int_num]);
     }
     return new_val;
+}
+
+void handler(int num)
+{
+    write(STDOUT_FILENO, "in function\n", 13);
+    exit_loop = 1;
+    close(SOCKETFD);
+    //exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char *argv[])
@@ -109,8 +121,9 @@ int main(int argc, char *argv[])
     /*
         3. Create socket to listen for request
     */
-    int socketfd = create_listen_socket(converted_port_num);
-    if (socketfd < 0) {
+    //int socketfd = create_listen_socket(converted_port_num);
+    SOCKETFD =  create_listen_socket(converted_port_num);
+    if (SOCKETFD < 0) {
         errx(1, "invalid socket number: %s ", argv[1]);
     }
     /*
@@ -123,25 +136,53 @@ int main(int argc, char *argv[])
     Sem_n_Queue* info = (Sem_n_Queue*)malloc(sizeof(Sem_n_Queue));
     info->semaphore = &num_of_requests;
     info->Q = Q;
+    info->exit_cond = &exit_loop;
     pthread_mutex_t audit_mutex;
     pthread_mutex_init(&audit_mutex, NULL);
+    
     /*
-        5. Set up worker threads
+        5. Set up Signal Handling
     */
-    pthread_t t;
-    pthread_t th[threads];    
-    for(int i = 0; i < 1; i++)
+   struct sigaction action;
+    memset(&action, 0, sizeof(struct sigaction));
+    action.sa_handler = handler;
+    sigaction(SIGTERM, &action, NULL);
+    //sigaction(SIGINT, &action, NULL);
+
+
+    /*
+        6. Set up worker threads
+    */
+    
+    //th = malloc(threads * sizeof(pthread_t));
+    pthread_t th[threads];
+    for(int i = 0; i < threads; i++)
     {
         if(pthread_create(&th[i], NULL, &startThread, (void*)info) != 0)
         {
             perror("Failed to create the thead");
         }
     }
-    while (true)
+    while (!exit_loop)
     {
         //Listen to Socket
         printf("waiting for connection\n");
-        int accept_socketfd = accept(socketfd, NULL, NULL);
+        if (exit_loop) {
+            close(SOCKETFD);  // Close the socket if termination is flagged
+            write(STDOUT_FILENO, "before\n", 8);
+            break;
+        }
+        int accept_socketfd = accept(SOCKETFD, NULL, NULL);
+        if (exit_loop) {
+            write(STDOUT_FILENO, "after\n", 7);
+            close(SOCKETFD);  // Close the socket if termination is flagged
+            break;
+        }
+        if (accept_socketfd == -1)
+        {
+            write(STDOUT_FILENO, "socket closed \n", 16);
+            break;
+        }
         int unique_id = rand() % 1000;
         //Create new task to send to queue
         Task t = {
@@ -156,16 +197,21 @@ int main(int argc, char *argv[])
         queue_push(Q, &t);
         queue_push(audit_queue, unique_id);
         sem_post(&num_of_requests);
+        //sleep(3);
     }
-    /*
+
+    write(STDOUT_FILENO, "BF LOOP \n ", 11);
     for(int i = 0; i < threads; i++)
     {
+        write(STDOUT_FILENO, "IN LOOP \n ", 11);
         if(pthread_join(th[i], NULL) != 0)
         {
             perror("Failed to join the thread");
         }
+        write(STDOUT_FILENO, "INN LOOP \n ", 11);
+
     }
-    */
+    write(STDOUT_FILENO, "hello world", 12);
     /*
         let all task finish
     */
